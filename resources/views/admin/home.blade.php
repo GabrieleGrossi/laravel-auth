b11a2e0f1d9f37a9e92997d2a8e0859cfeb2100a
GabrieleGrossi <grossigabriele98@gmail.com>
2023-08-24 09:29:10 +0200

    Add button on login home

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -6,7 +6,6 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
-
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
@@ -16,6 +15,14 @@
 
                     {{ __('You are logged in!') }}
                 </div>
+            </div>
+            <div>
+                <p>
+                    Press here to see all the project i made
+                </p>
+                <a href="{{ route('admin.projects.index')}}" class="btn btn-sm btn-success">
+                    Click here
+                </a>
             </div>
         </div>
     </div>
